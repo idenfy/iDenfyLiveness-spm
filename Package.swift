ca189cc4f6941131d4f439b56a53cbfffd874154
Bitrise CI <noreply@bitrise.io>
2025-11-24 06:38:20 +0000

    new version

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 
 import PackageDescription
 
-let version = "8.7.3"
+let version = "8.7.6"
 
 enum Checksums {
-    static let iDenfyInternalLoggerChecksum = "0d7172488d6d54bf6d7af1f8395b2b5bef1a17c554752a5f0eda28e039a0fb74"
-    static let FaceTecSDKChecksum = "73d92d1ff34cea488de6d60277b0f086c2e82fa04f1d1ac78f3149ed172c60c9"
-    static let iDenfyLivenessChecksum = "818fb8dc6cc4f9187014ed2909e0cbcabd3e395d27cac1096c8cfbc9b34f1718"
-    static let idenfyviewsChecksum = "6ac0eeed8d561e90e039ca5f055baa1b1bb4c4e8a5878023403cc838fa5d2e57"
-    static let iDenfySDKChecksum = "c5c9749750c0f9edae9f4804131fa4529c34e467f96f7301d1f031b8abffa564"
-    static let idenfycoreChecksum = "9a2bc0e2abd31b7fc839fed9cd560da6f91b124e760f440a68e28ee0ecbf6c4d"
+    static let iDenfyInternalLoggerChecksum = "e1be7d475c9183d27569bea7abcbba644cfcd7f9bf9c2fe46f453c1e0108e5d1"
+    static let FaceTecSDKChecksum = "aa9be292b5717a1237824ba88c895b69a1b1b47d671126d314a2400a75875f87"
+    static let iDenfyLivenessChecksum = "ad4edb1690fde430608cdc64737e6bde7cd239f39be2f45fe36ca5648e3f7923"
+    static let idenfyviewsChecksum = "566d6d066264e84656638688dadcb7bff4efd01b42cf4921e45c615c3605762c"
+    static let iDenfySDKChecksum = "8bcfa9a0b68d84b93ea097aabf6d16875bd3a4d3e1a8a0da0acb4632d01e7f56"
+    static let idenfycoreChecksum = "cb927dd69ccd5918b1f0fc859d664235ae93d6f102a6499a5f9710bdd979ff18"
 }
 
 let package = Package(
